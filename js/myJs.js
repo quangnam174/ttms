3d8066abd6e1847f8a00dea6a90a59bb0d56bb82
quangnam174 <89301731+quangnam174@users.noreply.github.com>
2025-05-12 03:40:34 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://www.facebook.com/quangnam.pham.1804";
+            window.location = "http://fb.com";
           },
         });
       }
